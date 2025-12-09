ce1084a4b09ae496d9aaa0d87062fd8cdfa2fa9b
m8test <contact@m8test.com>
2025-12-09 14:34:08 +0800

    feat: 升级了jdk、m8test以及idea插件相关版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,16 +32,16 @@ version = providers.gradleProperty("pluginVersion").get()
 
 // Set the JVM language level used to build the project.
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<JavaCompile> {
-    options.release.set(17)
+    options.release.set(21)
 }
 
 // Configure project's dependencies
